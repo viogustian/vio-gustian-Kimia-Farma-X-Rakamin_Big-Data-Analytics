a8306cd3fc881a38aace5f5288ba8bbce51051ff
viogustian <80340260+viogustian@users.noreply.github.com>
2024-03-03 02:08:48 +0700

    Update query_Kimia_Farma.sql

--- query_Kimia_Farma.sql
+++ query_Kimia_Farma.sql
@@ -1,3 +1,4 @@
+/* membuat tabel untuk analisa*/
 SELECT 
 transaction_id, 
 date, 
@@ -10,13 +11,15 @@ customer_name,
 product_name, 
 `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price AS actual_price, 
 discount_percentage,
-CASE
+
+CASE /* membuat kondisi */
   WHEN `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price <= 50000 THEN 0.10
   WHEN `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price > 50000 AND `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price <=100000 THEN 0.15
   WHEN `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price > 100000 AND `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price <=300000 THEN 0.20
   WHEN `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price > 300000 AND `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price <=500000 THEN 0.25
   WHEN `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price > 500000 THEN 0.30
 END AS persentase_gross_laba,
+
 CASE
   WHEN `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price <= 50000 THEN (1.00-discount_percentage)*(1.10*`rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price)
   WHEN `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price > 50000 AND `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price <=100000 THEN (1.00-discount_percentage)*(1.15*`rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price)
@@ -32,6 +35,9 @@ CASE
   WHEN `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price > 500000 THEN (1.00-discount_percentage)*(1.30*`rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price)-`rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.price
 END AS nett_profit,
 `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.rating AS rating_transaksi
+
+/* menggabungkan data dari table lain dengan fungsi join */
+
 FROM `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction` 
 JOIN `rakamin-kf-analytics-416005.kimia_farma.kf_kantor_cabang`
 ON `rakamin-kf-analytics-416005.kimia_farma.kf_final_transaction`.branch_id = `rakamin-kf-analytics-416005.kimia_farma.kf_kantor_cabang`.branch_id
